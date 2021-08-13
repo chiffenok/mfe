ee38dcb6a805e0d75ae7859dc5b8a4b5acaea493
chiffenok <apresnyakova88@gmail.com>
2021-08-13 21:28:16 +0200

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+    return (
+        <>
+        <h1>
+            hi
+        </h1>
+        <MarketingApp />
+        </>
+    )
+}
