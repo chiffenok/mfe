1d860a42efec0d4bee60f398ddff5dbeb8c000d1
chiffenok <apresnyakova88@gmail.com>
2021-09-10 18:17:13 +0200

    Implementing Multi-Tier Navigation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {createMemoryHistory, createBrowserHistory} from 'history'
 import App from './App'
 
-const mount = (el) => {
+const mount = (el, {onNavigate, defaultHistory}) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate)
+    }
+    
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     )
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const { pathname } = history.location
+            if (pathname !== nextPathname) {
+                history.push(nextPathname)
+            }
+        }
+    }
 }
 
 if(process.env.NODE_ENV === 'development') {
@@ -15,7 +31,7 @@ if(process.env.NODE_ENV === 'development') {
     
     if(devRoot) {
         console.log(process.env.NODE_ENV)
-        mount(devRoot)
+        mount(devRoot, {defaultHistory: createBrowserHistory()})
     }
 }
 
